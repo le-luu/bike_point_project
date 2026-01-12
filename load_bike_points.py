6f4b4f1e15dafffc1a1829136b2486565e1a3932
Le Luu <le.luu.dev@gmail.com>
2026-01-12 11:05:54 +0000

    Added loading to s3 script

--- load_bike_points.py
+++ load_bike_points.py
@@ -1,6 +1,7 @@
 import boto3
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
 
@@ -8,7 +9,18 @@ AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")  
 
+logs_dir = "logs"
+
 filepath = "data/2026-01-07_15-29-03.json"
+
+log_filename = f"logs/logging_example_2026-01-07_15-29-03.log"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    filename=log_filename
+)
+logger = logging.getLogger()
+
 print(AWS_BUCKET_NAME)
 
 s3_client = boto3.client('s3', 
@@ -31,8 +43,12 @@ for (root,dirs,files) in os.walk(data_path,topdown=True):
         if len(files)>0 and file.endswith('.json'):
             try:
                 s3_client.upload_file(os.path.join(root,file), AWS_BUCKET_NAME, file)
+                os.remove(os.path.join(root,file))
                 print(f"Uploaded {file} to S3 bucket {AWS_BUCKET_NAME} successfully.")
+                logger.info(f"Uploaded {file} to S3 bucket {AWS_BUCKET_NAME} successfully.")
             except Exception as e:
                 print(f"Failed to upload {file}. Error: {e}")
+                logger.error(f"Failed to upload {file}. Error: {e}")
         else:
             print("There are no JSON files to upload.")
+            logger.warning("There are no JSON files to upload.")
